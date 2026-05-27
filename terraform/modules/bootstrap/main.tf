80655a8f856904f2bc9dbb62749efcc070c956b7
Maschior <matheus.rubens.ti@gmail.com>
2026-05-27 10:32:15 -0300

    style: format terraform files

--- terraform/modules/bootstrap/main.tf
+++ terraform/modules/bootstrap/main.tf
@@ -1,49 +1,49 @@
 resource "aws_s3_bucket" "terraform_state" {
-    bucket = "${substr(var.domain_name, 0, length(var.domain_name) - 4)}-terraform-state"
+  bucket = "${substr(var.domain_name, 0, length(var.domain_name) - 4)}-terraform-state"
 
-    tags = {
-        Name = "terraform-state" 
-    }
+  tags = {
+    Name = "terraform-state"
+  }
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
-    bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.terraform_state.id
 
-    versioning_configuration {
-        status = "Enabled"
-    }
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-    bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.terraform_state.id
 
-    rule {
-        apply_server_side_encryption_by_default {
-          sse_algorithm = "AES256"
-        }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "terraform_state" {
-    bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.terraform_state.id
 
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-    name         = "terraform-locks"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key     = "LockID"
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 
-    tags = {
-        Name = "terraform-locks"
-    }
+  tags = {
+    Name = "terraform-locks"
+  }
 }
--- terraform/modules/bootstrap/variables.tf
+++ terraform/modules/bootstrap/variables.tf
@@ -1,9 +1,9 @@
-variable aws_region {
+variable "aws_region" {
   description = "The AWS region to deploy resources in."
   type        = string
 }
 
-variable domain_name {
-    description = "The domain name to use for the S3 bucket."
-    type        = string
+variable "domain_name" {
+  description = "The domain name to use for the S3 bucket."
+  type        = string
 }
