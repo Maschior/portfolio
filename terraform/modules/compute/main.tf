55aaaa6d95b621333bf19d858efb3c5be2b65393
Maschior <matheus.rubens.ti@gmail.com>
2026-05-28 10:26:35 -0300

    feat: bind ansible folder directly to EC2 bootstrap and enable auto-recreate

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -26,9 +26,14 @@ resource "aws_instance" "this" {
   user_data = templatefile(
     "${path.module}/scripts/bootstrap.sh.tpl",
     {
-      tunnel_token = var.tunnel_token
+      tunnel_token     = var.tunnel_token
+      ansible_playbook = file("${path.module}/../../../ansible/playbook.yml")
+      ansible_config   = file("${path.module}/../../../ansible/ansible.cfg")
+      ansible_hosts    = file("${path.module}/../../../ansible/hosts")
     }
   )
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "${var.project_name}-instance"
