5ffd80cc2ad3115630f0693d765aa7e53e35071c
Maschior <matheus.rubens.ti@gmail.com>
2026-06-14 15:56:23 -0300

    reset

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = ">= 1.5"
 
-  # backend "s3" {
-  #   bucket         = "maschior-terraform-state"
-  #   key            = "state/terraform.tfstate"
-  #   region         = "sa-east-1"
-  #   dynamodb_table = "terraform-locks"
-  #   encrypt        = true
-  # }
+  backend "s3" {
+    bucket         = "maschior-terraform-state"
+    key            = "state/terraform.tfstate"
+    region         = "sa-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 
   required_providers {
     aws = {
